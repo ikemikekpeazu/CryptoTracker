c9e06a29596241b9045c482cdb29a7a132b66b84
Ikem <iikekpeazu@gmail.com>
2026-03-19 16:46:30 -0400

    Initial Commit

---
+++ SwiftfulCrypto/SwiftfulCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulCryptoApp.swift
+//  SwiftfulCrypto
+//
+//  Created by Ikem Ikekpeazu on 3/19/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
